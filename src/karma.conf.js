71f6f7cca36208027f86a1d746d8b0773b8b079a
Fabian Beyerlein <dev@dazeeeh.com>
2018-06-24 21:37:18 +0200

    karma config and clang-format

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,31 +1,38 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-jasmine'), require('karma-chrome-launcher'), require('karma-firefox-launcher'),
+      require('karma-jasmine-html-reporter'), require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma'), require('karma-spec-reporter')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false  // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../coverage'),
       reports: ['html', 'lcovonly'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml'],
+    specReporter: {
+      maxLogLines: 5,               // limit number of lines logged per test
+      suppressErrorSummary: false,  // print error summary
+      suppressFailed: false,        // do not print information about failed tests
+      suppressPassed: false,        // do not print information about passed tests
+      suppressSkipped: true,        // do not print information about skipped tests
+      showSpecTiming: true,         // print the time elapsed for each spec
+      failFast: true                // test would finish with error when a first fail occurs.
+    },
+    reporters: ['spec', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    browsers: ['Chrome', 'Firefox'],
+    singleRun: true
   });
 };
